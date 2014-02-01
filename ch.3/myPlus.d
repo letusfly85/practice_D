7d22ace898a11d6c7ebb41997cca9adc22fac898
letusfly85 <letusfly95@gmail.com>
2014-02-02 01:32:43 +0900

    add operand

--- ch.3/myPlus.d
+++ ch.3/myPlus.d
@@ -7,4 +7,31 @@ void writeType(T, string op, U)()
 
 void main() {
     writeType!( byte, "+",  byte)();
+    writeType!( byte, "+",  ubyte)();
+    writeType!( ubyte, "+",  ubyte)();
+
+    writeln();
+
+    writeType!( short, "+", short)();
+    writeType!( short, "+", ushort)();
+    writeType!( ushort, "+", ushort)();
+
+    writeln();
+
+    writeType!( int, "+", int)();
+    writeType!( int, "+", uint)();
+    writeType!( uint, "+", uint)();
+
+    writeln();
+
+    writeType!( long, "+", long)();
+    writeType!( long, "+", ulong)();
+    writeType!( ulong, "+", ulong)();
+
+    writeln();
+
+    writeType!( int , "+", long)();
+    writeType!( int , "+", ulong)();
+    writeType!( uint, "+", long)();
+    writeType!( uint, "+", ulong)();
 }
