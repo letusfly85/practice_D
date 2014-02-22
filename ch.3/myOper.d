27e44270c1c7febe78160dcb28bedc79f3aa9a52
letusfly85 <letusfly95@gmail.com>
2014-02-22 23:33:48 +0900

    assing

--- ch.3/myOper.d
+++ ch.3/myOper.d
@@ -7,9 +7,32 @@ void main() {
 
     writeln(2.3 ^^ 4.4);
 
-    int a   = 4;
+    int c   = 4;
     int b33 = 33;
 
-    writeln(a << 0);
-    writeln(a << 1);
+    writeln(c << 0);
+    writeln(c << 1);
+
+    int a;
+    writeln(a);
+
+    a = 12;
+    writeln(a);
+
+    a += 12;
+    writeln(a);
+
+    a -= 12;
+    writeln(a);
+
+    a *= a;
+    writeln(a);
+
+    a /= a / 4;
+
+    a %= 3;
+    writeln(a);
+
+    ++a ^^= 3;
+    writeln(a);
 }
